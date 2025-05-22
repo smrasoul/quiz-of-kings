dc167c036523513d09cc0a60d1a855021018400e
Rasoul <smrasoul76@gmail.com>
2025-05-22 13:07:01 +0330

    Using Bootstrap 5 instead of tailwind

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,7 @@
+import '../sass/app.scss';
+import * as bootstrap from 'bootstrap';
+
+
 import axios from 'axios';
 window.axios = axios;
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +10,10 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+        }
+    },
 });
